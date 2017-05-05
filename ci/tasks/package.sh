04a69d2dab8eae042a2473fdd88ebc05bc2e6a6d
murtazacg <murtaza.goga@capgemini.com>
2017-05-05 15:18:23 -0400

    foo

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+echo "1Done packaging"
 set -e +x
-
+echo "2Done packaging"
 pushd movie-fun
   echo "Packaging JAR"
   ./mvnw clean package -DskipTests
 popd
-
+echo "3Done packaging"
 jar_count=`find movie-fun/target -type f -name *.war | wc -l`
 
 if [ $jar_count -gt 1 ]; then
