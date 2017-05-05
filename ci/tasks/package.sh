cf17d73f7775dccec99e11001d2d9f684aea961a
murtazacg <murtaza.goga@capgemini.com>
2017-05-05 14:53:22 -0400

    foo

---
+++ ci/tasks/package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e +x
+
+pushd movie-fun
+  echo "Packaging JAR"
+  ./mvnw clean package -DskipTests
+popd
+
+jar_count=`find attendee-service/target -type f -name *.war | wc -l`
+
+if [ $jar_count -gt 1 ]; then
+  echo "More than one war found, don't know which one to deploy. Exiting"
+  exit 1
+fi
+
+find movie-fun/target -type f -name *.war -exec cp "{}" package-output/movie-fun.war \;
+
+echo "Done packaging"
+exit 0
